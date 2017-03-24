3de4f35a5db9c328d6cdccaace688aa3e00a3f8a
Boris FELD <lothiraldan@gmail.com>
2017-03-24 18:38:55 +0100

    Add ignoring vendor and remove jekyll call from gulfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp        = require('gulp'),
 	plumber     = require('gulp-plumber'),
-	browserSync = require('browser-sync'),
 	stylus      = require('gulp-stylus'),
 	uglify      = require('gulp-uglify'),
 	concat      = require('gulp-concat'),
@@ -15,35 +14,6 @@ var messages = {
 	jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
 };
 
-var jekyllCommand = (/^win/.test(process.platform)) ? 'jekyll.bat' : 'jekyll';
-
-/**
- * Build the Jekyll Site
- */
-gulp.task('jekyll-build', function (done) {
-	browserSync.notify(messages.jekyllBuild);
-	return cp.spawn(jekyllCommand, ['build'], {stdio: 'inherit'})
-		.on('close', done);
-});
-
-/**
- * Rebuild Jekyll & do page reload
- */
-gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
-	browserSync.reload();
-});
-
-/**
- * Wait for jekyll-build, then launch the Server
- */
-gulp.task('browser-sync', ['jekyll-build'], function() {
-	browserSync({
-		server: {
-			baseDir: '_site'
-		}
-	});
-});
-
 /**
  * Stylus task
  */
@@ -55,7 +25,6 @@ gulp.task('stylus', function(){
 			compress: true
 		}))
 		.pipe(gulp.dest('_site/assets/css/'))
-		.pipe(browserSync.reload({stream:true}))
     .pipe(gulp.dest('assets/css'));
 });
 
@@ -68,7 +37,6 @@ gulp.task('js', function(){
 		.pipe(concat('main.js'))
 		.pipe(uglify())
 		.pipe(gulp.dest('assets/js/'))
-		.pipe(browserSync.reload({stream:true}))
     .pipe(gulp.dest('_site/assets/js/'));
 });
 
@@ -97,4 +65,4 @@ gulp.task('watch', function () {
  * Default task, running just `gulp` will compile the stylus,
  * compile the jekyll site, launch BrowserSync & watch files.
  */
-gulp.task('default', ['js', 'stylus', 'browser-sync', 'watch']);
+gulp.task('default', ['js', 'stylus', 'watch']);
